91e7471f2d8e1b87e239db1bb20aa9ed6fccbbad
emanueljg <emanueljohnsongodin@gmail.com>
2023-03-27 01:43:18 +0200

    Revert "Update flake.nix"
    This reverts commit 952394e0c0a7e02f4c410e61c45c3a5b6170e766.

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       pkg = (pkgs.buildNpmPackage {
         inherit name;
         src = ./.;
-        npmDepsHash = "sha256-QQSzCBQ2RG2kJggqU/f+VQHzlJqwZpdqkz4tckdobp=";
+        npmDepsHash = "sha256-QQSzCBQ2RG2kJggqU/f+VQHzlJqwZpdqkz4tckdobpQ=";
         dontNpmBuild = true;
       });
 
